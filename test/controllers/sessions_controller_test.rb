f86e04f31b44aaa22e0f09bd0df468c363bb7e82
Aditya <aditya.s@browserstack.com>
2017-01-26 00:12:17 +0530

    added sessions controller tests

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -1,19 +1,49 @@
 require 'test_helper'
 
 class SessionsControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
-    get sessions_new_url
+  def setup
+    DatabaseCleaner.clean
+    @password = "123456"
+    @user = create :user, password: @password, password_confirmation: @password
+  end
+
+  def test_must_get_new_on_login
+    get login_url
+
+    assert_kind_of User, assigns(:user)
     assert_response :success
   end
 
-  test "should get create" do
-    get sessions_create_url
+  def test_must_login_user_on_sessions_create
+    post sessions_url, params: {session: {email: @user.email, password: @password}}
+
+    assert_redirected_to root_url
+  end
+
+  def test_must_not_login_user_on_sessions_create_when_email_is_invalid
+    post sessions_url, params: {session: {email: "test@sample.com", password: @password}}
+
+    assert_template :new
     assert_response :success
   end
 
-  test "should get destroy" do
-    get sessions_destroy_url
+  def test_must_not_login_user_on_sessions_create_when_password_is_invalid
+    post sessions_url, params: {session: {email: @user.email, password: "test"}}
+
+    assert_template :new
     assert_response :success
   end
 
+  def test_must_logout_current_user_on_sessions_destroy
+    sign_in(@user)
+    delete logout_url
+
+    assert_redirected_to login_url
+  end
+
+  def test_must_redirect_to_login_page_on_destroy_when_user_not_logged_in
+    delete logout_url
+
+    assert_redirected_to login_url
+  end
 end
