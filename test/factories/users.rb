43383d568e7cc52c0f03f0a0974f9d5f2a4a33d4
Aditya <aditya.s@browserstack.com>
2017-01-25 18:13:08 +0530

    added user model test cases

--- test/factories/users.rb
+++ test/factories/users.rb
@@ -2,5 +2,10 @@ FactoryGirl.define do
   factory :user do
     name Faker::Name.name
     email Faker::Internet.email
+
+    trait :with_password do
+      password "123456"
+      password_confirmation "123456"
+    end
   end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,48 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = build :user
+  end
+
+  def test_user_is_invalid_if_email_is_empty
+    @user.email = nil
+    assert_equal false, @user.valid?
+    assert_equal ["can't be blank", "is not a valid email address"], @user.errors[:email]
+  end
+
+  def test_user_is_invalid_if_email_is_invalid
+    @user.email = "aditya"
+    assert_equal false, @user.valid?
+    assert_equal ["is not a valid email address"], @user.errors[:email]
+  end
+
+  def test_user_is_invalid_if_password_is_empty
+    assert_equal false, @user.valid?
+    assert_equal ["can't be blank", "is too short (minimum is 6 characters)"], @user.errors[:password]
+  end
+
+  def test_user_is_invalid_if_password_is_invalid
+    @user.password = "123"
+    assert_equal false, @user.valid?
+    assert_equal ["is too short (minimum is 6 characters)"], @user.errors[:password]
+  end
+
+  def test_user_is_invalid_if_password_confirmation_is_empty
+    @user.password = "123456"
+    assert_equal false, @user.valid?
+    assert_equal ["can't be blank"], @user.errors[:password_confirmation]
+  end
+
+  def test_user_is_invalid_if_password_confirmation_is_invalid
+    @user.password = "123456"
+    @user.password_confirmation = "123"
+    assert_equal false, @user.valid?
+    assert_equal ["doesn't match Password"], @user.errors[:password_confirmation]
+  end
+
+  def test_user_is_valid
+    @user = build :user, :with_password
+    assert_equal true, @user.valid?
+  end
 end
