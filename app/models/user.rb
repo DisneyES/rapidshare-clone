4e57277f01452dbf1179b331ce81b279445a5bc6
Aditya <aditya.s@browserstack.com>
2017-01-25 16:06:01 +0530

    added user 'has_many' association for uploads

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
   has_secure_password
 
   validates :email, presence: true, uniqueness: true, email: true
-  validates :password, confirmation: true, length: {minimum: 6}
+  validates :password, confirmation: true, length: {minimum: 6}, on: :create
   validates :password_confirmation, presence: true, if: 'password.present?'
+
+  has_many :uploads
 end
