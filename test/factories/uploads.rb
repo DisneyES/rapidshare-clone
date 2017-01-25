47094c3e93995ede20f5f9d9851c1fc5a1bd6acc
Aditya <aditya.s@browserstack.com>
2017-01-25 18:16:11 +0530

    added upload model test cases

--- test/factories/uploads.rb
+++ test/factories/uploads.rb
@@ -1,5 +1,10 @@
 FactoryGirl.define do
   factory :upload do
-    
+    access_token Faker::Number.hexadecimal(10)
+    association :user
+
+    trait :with_file do
+      file { Rack::Test::UploadedFile.new("test/fixtures/sample.png", "image/png") }
+    end
   end
 end
--- test/models/upload_test.rb
+++ test/models/upload_test.rb
@@ -1,7 +1,29 @@
 require 'test_helper'
 
 class UploadTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    DatabaseCleaner.clean
+    @user = create :user, :with_password
+    @upload = build :upload, :with_file, user: @user
+  end
+
+  def test_upload_is_invalid_if_file_is_empty
+    upload = build :upload, user: @user
+
+    assert_equal false, upload.valid?
+    assert_equal ["can't be blank"], upload.errors[:file]
+  end
+
+  def test_upload_is_invalid_if_access_token_is_empty_on_update
+    @upload.save
+    @upload.access_token = nil
+
+    assert_equal false, @upload.valid?
+    assert_equal ["can't be blank"], @upload.errors[:access_token]
+  end
+
+  def test_upload_is_valid
+    assert_equal true, @upload.save
+  end
 end
+
