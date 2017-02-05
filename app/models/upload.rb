9aa879e11fcdf01cdce95236ed3178e508090440
Aditya <aditya.s@browserstack.com>
2017-02-05 23:23:12 +0530

    added helper methods for carrierwave filename and content_type in upload model

--- app/models/upload.rb
+++ app/models/upload.rb
@@ -9,8 +9,16 @@ class Upload < ApplicationRecord
   validates :file, presence: true
   validates :access_token, presence: true, uniqueness: true, on: :update
 
+  def filename
+    self.file.file.filename
+  end
+
+  def content_type
+    self.file.file.content_type
+  end
+
   def exported_name
-    fname = self.file.file.filename
+    fname = filename
     extn = File.extname(fname)
     extn.slice!(0)
     BLACKLISTED_EXTENSIONS.include?(extn) ? "#{fname}.txt" : fname
